eba1aa15c7b8478a679d7005d4f834d3fdacebb9
Michele_Manco <motlamik@gmail.com>
2019-12-17 11:42:06 +0100

    key press+hb's working

--- main.js
+++ main.js
@@ -1,41 +1,15 @@
   var api_url_base = 'https://api.themoviedb.org/3';
 $(document).ready(function(){
 $('.slabel').click(function(){
-  // var searchy = $('#sfilter').val();
-  // $('#sfilter').val('');
+
   userSearch();
-  // $.ajax({
-  //   'url': api_url_base + '/search/movie',
-  //   'data':{
-  //     'api_key':'1c2382ce77d5c3384075b987fc84ac1a',
-  //     'query': 'star wars'
-  //   },
-  //   'method': 'GET',
-  //   'success': function(data) {
-  //     var film = data.results;
-  //     for (var i = 0; i <film.length; i++) {
-  //         var searched_movie = film[i];
-  //         var titolo = searched_movie.title;
-  //         var titolo_originale = searched_movie.original_title;
-  //         var lingua = searched_movie.original_language;
-  //         var voto = searched_movie.vote_average;
-  //         console.log(titolo);
-  //
-  //     }
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //   },
-  //   'error': function(){
-  //     alert('errore');
-  //
-  //   }
-  // });
+
 })
+$(document).on('keypress', '.searchbar-input', function(){
+     if(event.which == '13'){
+           userSearch();
+     }
+   });
 function userSearch(){
 $('.movies').remove();
 var source = $("#movies-template").html();
@@ -58,6 +32,13 @@ if (searchy.length != 0) {
           var lingua = searched_movie.original_language;
           var voto = searched_movie.vote_average;
           console.log(searched_movie);
+          var source = {
+          titolo: titolo,
+          titolo_originale: titolo_originale,
+          lingua : lingua,
+          voto: voto
+
+          };
 
 
       }
@@ -76,10 +57,3 @@ $('sfilter').val('');
 }
 
 });
-// var source = {
-//   title: titolo,
-//   original_title: titolo_originale,
-//   language : lingua,
-//   rating: voto
-//
-// };
